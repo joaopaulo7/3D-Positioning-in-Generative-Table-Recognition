ca256737669207c42fa2353b24bcf7fd51cd7783
joaopaulo7 <joaopauloff7@hotmail.com>
2024-05-18 12:57:03 -0300

    Adapt preprocess

--- aux/prepare_data.sh
+++ aux/prepare_data.sh
@@ -7,3 +7,7 @@ tar -C data/ -xf data/pubtabnet.tar.gz  pubtabnet/
 mv data/pubtabnet data/imgs
 
 mv data/imgs/PubTabNet_2.0.0.jsonl data/
+
+mkdir data/anns data/anns/train data/anns/test data/anns/val
+
+
